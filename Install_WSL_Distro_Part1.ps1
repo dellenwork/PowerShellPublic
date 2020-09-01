d645675cb7c8a440a1a85305e6cf5133bde09c00
dellenwork <49877041+dellenwork@users.noreply.github.com>
2020-08-31 22:12:57 -0400

    Add files via upload
    Unattended WSL and Distro Install and configure

---
+++ Install_WSL_Distro_Part1.ps1
@@ -0,0 +1,65 @@
+﻿############## Downloading and installing the app ###################
+$ErrorActionPreference = 'Continue'
+
+# Enable wsl subsystems for linux (Must run in admin mode)
+#Enable-WindowsOptionalFeature -Online -FeatureName VirtualMachinePlatform
+#Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Windows-Subsystem-Linux
+
+# Set Tls12 protocol to be able to download the wsl application
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+
+# Set variables
+$Default_Password = "password"
+$DistroName = "wsl-debian-gnulinux"
+#$DistroName = "wsl-ubuntu-1804"
+$Distro = $DistroName.split("-")[1] #Split the name on dashes to use the 2nd element for the .exe
+$Distro_URL = "https://aka.ms/" + $DistroName
+$pkgpath = "c:\Users\Public\"
+#$username = "student"  # Hard coded username to create path and user in the distro
+$username = $env:UserName
+
+# check to see if distro installation file exists or download the app otherwise
+$FileToGet = $pkgpath + $Distro + ".appx"
+if ( Test-Path $FileToGet -PathType leaf ) { "File already exists, deleting"
+    # Cleanup after the install and remove installer
+    Remove-Item -path $FileToGet }
+# Download installer
+(new-object System.Net.WebClient).DownloadFile($Distro_URL,$FileToGet) 
+
+Write-Host "Starting appx install"
+# Actually install the wsl distro app
+$mycmd = "Add-AppxPackage " + $FileToGet 
+invoke-expression -Command $mycmd
+Write-Output "Installed $Distro : $?"
+
+############## Initializing the wsl distro app without requiring user input ###################
+
+# First define path to the installed distro
+$Path1 = "/Users/"
+$Path2 = "/AppData/Local/Microsoft/WindowsApps/" + $Distro + ".exe"
+$hdd_name=(Get-WmiObject Win32_OperatingSystem).SystemDrive
+[String] $distro_path = $hdd_name + $Path1 + $username + $Path2
+
+# Install and set default user initially to root
+$Option1 = " install --root"
+$InstallCmd = $Distro + $Option1
+
+#Set up a job so I can timeout the installer after a set period of time becuase the job will hang 
+#waiting for input that is not provided.
+$Job = Start-Job -ScriptBlock {
+        Invoke-Command -ComputerName localhost -ScriptBlock {
+        invoke-expression -Command $InstallCmd 
+        } 
+}
+$Job | Wait-Job -Timeout 310
+$Job | Stop-Job
+
+# Cleanup after the install and remove installer
+Remove-Item -path $FileToGet
+
+############ Set up for part 2 after reboot    #######################################
+
+# Set registry for RunOnce after reboot
+
+
+#Restart-Computer  # WSL requires a reboot
---
+++ Install_WSL_Distro_Part2.ps1
@@ -0,0 +1,76 @@
+﻿############## Downloading and installing the app ###################
+$ErrorActionPreference = 'Continue'
+
+# Set variables
+$Default_Password = "password"
+$DistroName = "wsl-debian-gnulinux"
+#$DistroName = "wsl-ubuntu-1804"
+$Distro = $DistroName.split("-")[1] #Split the name on dashes to use the 2nd element for the .exe
+$Distro_URL = "https://aka.ms/" + $DistroName
+$pkgpath = "c:\Users\Public\"
+#$username = "student"  # Hard coded username to create path and user in the distro
+$username = $env:UserName
+
+
+############ Configure installed distro    #######################################
+           
+# set path
+#$env:Path = $env:Path + ';c:\windows\system32'
+
+# Set as the default distro
+.\wslconfig.exe /setdefault $Distro
+write-host "installed default $?"
+
+# Set WSL to V2 for better performance
+wsl --set-default-version 2
+write-host "installed version 2 $?"
+
+# Create user  in Distro       -p $(echo $Default_password | openssl passwd -1 -stdin)
+$MyCmd4 = "$Distro run useradd -g adm -d /home/$username -s /bin/bash  $username"
+Invoke-Expression -Command $MyCmd4
+write-host "installed user $?"
+
+# set password
+$StrUser = $username + ":" + $Default_Password
+$MyCmd2 = $Distro + " run echo ""$StrUser"" | /usr/sbin/chpasswd"
+Invoke-Expression -Command $MyCmd2
+write-host "installed password $?"
+#$SetPW = "$Distro run echo "D:password" | chpasswd"
+#Invoke-Expression $SetPW
+
+# Add user to distro Sudoers
+$MyCmd5 = " $Distro run usermod -aG sudo $username "
+Invoke-Expression -Command $MyCmd5
+write-host "add to sudoers $?"
+
+
+
+# Create user account, add to sudoer group for distro and set password
+#$MyCmd = $Distro + " run adduser -m -s /bin/bash  -G sudo $username"
+#Invoke-Expression -Command $MyCmd
+
+
+
+#$Distro run $(echo $Default_password | passwd --stdin $username)
+
+
+
+# Add code to refresh $Distro repo and get python packages
+$cmd = "$Distro run apt-get -y update "
+Invoke-expression $Cmd\
+write-host "installed updates $?"
+
+$cmd = "$Distro run  sudo apt-get -y Upgrade"
+Invoke-expression $Cmd
+if ($? -eq 0) {write-host "installed upgrades" }
+            #$Distro run sudo apt-get dist-upgrade
+            #$Distro run  sudo apt-get autoremove
+            #$Distro run  sudo apt-get install git-all
+            #$Distro -c  sudo apt-get install software-properties-common python-software-properties
+            # $Distro -c  sudo apt-get install python3
+
+
+# Change default user in Distro
+Invoke-Expression -Command "$Distro config --default-user $username"
+
+"Done"
